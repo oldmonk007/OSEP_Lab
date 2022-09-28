b8feee113caa8f1e053463cc240d745cd5807152
oldmonk <oldmonk000007@gmail.com>
2022-09-28 00:30:06 -0400

    first commit

---
+++ Add_Type3511.ps1
@@ -0,0 +1,13 @@
+﻿$User32 = @"
+using System;
+using System.Runtime.InteropServices;
+public class User32 {
+[DllImport("user32.dll", CharSet=CharSet.Auto)] public static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+[DllImport("advapi32.dll", SetLastError = true)]public static extern bool GetUserName(System.Text.StringBuilder sb, ref Int32 length);
+}
+"@
+Add-Type $User32
+$name_len = 64
+$user_name = New-Object System.Text.StringBuilder -Argumentlist $name_len
+[User32]::GetUserName($user_name, [ref]$name_len)
+[User32]::MessageBox(0, $user_name, "MyBox", 0)
